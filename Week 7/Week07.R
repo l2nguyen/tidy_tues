f0ed50d3429fde259d43e029a6c1130d12ecabeb
l2nguyen <lena.nguyen4@gmail.com>
2018-05-16 17:42:58 -0400

    Cleaning up of column names

--- Week 7/Week07.R
+++ Week 7/Week07.R
@@ -1,15 +1,29 @@
 library("tidyverse")
 library("here")
 
-data <- read_csv(here("StarWars.csv"), col_names=TRUE)
+data <- read_csv(here("StarWars.csv"), col_names = TRUE)
 
 #---- CLEAN DATA ------#
 # clean up column names
 colnames(data) <- paste(colnames(data), data[1,])
-colnames(data) <- gsub("^X[0-9]", "", colnames(data))
-colnames(data) <- gsub("Response$", "", colnames(data))
+colnames(data) <- gsub("^X[0-9][0-9]? ", "", colnames(data))
+colnames(data) <- gsub(" Response$", "", colnames(data))
 
-# remove first column
+# Manual cleaning up of column names
+names(data)[4] <- "Star Wars: Episode I The Phantom Menace"
+names(data)[10] <- "Star Wars: Episode I  The Phantom Menace"
+names(data)[16] <- "Hans Solo"
+names(data)[32] <- "Fan of the Expanded Universe"
+names(data)[33] <- "Star trek fan"
+names(data)[10:15] <- paste(names(data)[10:15], "Rank")
+
+# drop first column
 data <- data[-1,]
 
 str(data)
+
+# Change character columns into factor for easier data manipulation
+data %>% mutate_if(is.character, as.factor) -> data
+
+
+
